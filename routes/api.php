a1bb62607c2339b4956b768a2fccc4050775116d
olamilekan <olamilekanomosanya@gmail.com>
2022-03-23 19:12:17 +0100

    solve the bug of target class does not exist

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('students', 'ApiController@getAllStudents');
-Route::get('students/{id}', 'ApiController@getStudent');
-Route::post('students', 'ApiController@createStudent');
-Route::put('students/{id}', 'ApiController@updateStudent');
-Route::delete('students/{id}','ApiController@deleteStudent');
+Route::get('students', 'App\Http\Controllers\ApiController@getAllStudents');
+Route::get('students/{id}', 'App\Http\Controllers\ApiController@getStudent');
+Route::post('students', 'App\Http\Controllers\ApiController@createStudent');
+Route::put('students/{id}', 'App\Http\Controllers\ApiController@updateStudent');
+Route::delete('students/{id}','App\Http\Controllers\ApiController@deleteStudent');
